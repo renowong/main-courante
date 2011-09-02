18c6adf2f21bbf81d659e836c1579f35e2c5d47b
rwong <rwong@1b0faf87-76a6-22d5-5442-9fc990a4f63f>
2011-09-02 20:32:06 +0000

    Removed css display block for tables.

--- includes/menu.php
+++ includes/menu.php
@@ -7,7 +7,7 @@ $menu .= "<li><a href='mc_list.php'>Retour Liste</a></li>";
 if($_SESSION['usertype']=='1'){$menu .= "<li><a href='agents.php'>Gestion des Agents</a></li>";}
 if($_SESSION['usertype']=='1'){$menu .= "<li><a href='extract.php'>Extractions</a></li>";}
 $menu .= "<li><a href='comptes.php'>Compte</a></li>";
-$menu .= "<li><a href='index.php'>Sortir</a></li>";
+$menu .= "<li><a href='index.php'>Quitter</a></li>";
 $menu .= "</ul>";
 $menu .= "</div>";
 
