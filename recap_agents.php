c293621cfcdb88baa2328badb58153e3ddc8108b
renowong@gmail.com <renowong@gmail.com@1b0faf87-76a6-22d5-5442-9fc990a4f63f>
2012-07-11 18:39:44 +0000

    css work

--- recap_agents.php
+++ recap_agents.php
@@ -10,10 +10,10 @@ function get_agents($eq){
     $mysqli = new mysqli(HOST, DBUSER, DBPASSWORD, DB);
   
     $query = "SELECT `agents`.`id_agent`, `agents`.`agent`, `equipes`.`designation` FROM `agents` INNER JOIN `equipes` ON `agents`.`equipe` = `equipes`.`id_equipe` WHERE `agents`.`radie` = '0' AND `equipes`.`id_equipe` = '$eq' ORDER BY `agent` ASC";
-    $output ="<table><th>Agent</th><th>Equipe</th><th>Action</th>";
+    $output ="<table><th>Agent</th><th class='center'>Equipe</th><th class='center'>Action</th>";
     if ($result = $mysqli->query($query)) {
         while($row = $result->fetch_assoc()){
-            $output .= "<tr id='".$row['id_agent']."'><td>".casefix($row['agent'])."</td><td>".$row['designation']."</td><td><a href='javascript:radier(\"".$row['id_agent']."\")'><img class='imgtrash' src='img/trash.png'>Radier</a></td></tr>";
+            $output .= "<tr id='".$row['id_agent']."'><td>".casefix($row['agent'])."</td><td class='center'>".$row['designation']."</td><td class='center'><a href='javascript:radier(\"".$row['id_agent']."\")'><img class='imgtrash' src='img/trash.png'></a></td></tr>";
         }
     }
     $result->free();
